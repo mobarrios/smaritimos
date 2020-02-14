71103addd9353b6244a0e9f8e7b655c14af5a685
Manuel Barrios <mbarrios@Manuels-MacBook-Pro.local>
2020-02-14 12:49:32 -0300

    upd

--- resources/views/admin/items/qr.blade.php
+++ resources/views/admin/items/qr.blade.php
@@ -6,8 +6,8 @@
 </div>	
 <hr>
 <div>
-	<small> Modelo :</small> <strong>{{$it->Models->name}}</strong><br>
-	<small>NSerie / IDent. :</small> <strong>{ {{$it->n_serie}}</strong><br>
+	<small> Modelo :</small> <strong>{{$it->Models->Brands->name}} {{$it->Models->name}}</strong><br>
+	<small>NSerie / IDent. :</small> <strong>{{$it->n_serie}}</strong><br>
 	<small>Vto. :</small> <strong>{{$it->f_vencimiento}}</strong><br>
 </div>
 </body>
