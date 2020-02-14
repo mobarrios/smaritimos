196833ec440461cc561f5aa38d27c4903948afa9
Manuel Barrios <mbarrios@Manuels-MacBook-Pro.local>
2020-02-14 12:22:04 -0300

    upd

--- resources/views/admin/items/qr.blade.php
+++ resources/views/admin/items/qr.blade.php
@@ -4,5 +4,10 @@
 <div style="padding: 5px">
 	{!!  DNS2D::getBarcodeHTML($it->id, "QRCODE") !!}
 </div>	
+<div>
+	<h3>{{$it->n_serie}}</h3>
+	<h3>{{$it->f_vencimiento}}</h3>
+	
+</div>
 </body>
 </html>
