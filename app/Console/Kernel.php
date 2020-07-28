56b8d9004e8d334e8221d8f8b3b5130f96d45c6a
Manuel Barrios <mbarrios@MBP-de-Manuel.fibertel.com.ar>
2020-07-28 12:57:00 -0300

    schedulee

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Console;
-
+use DB;
+use App\Http\Controllers\Admin\ItemsController as itemsController; 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +25,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        //$schedule->command('inspire')
+         //        ->hourly();
+
+         $schedule->call(function(){
+
+            $itemsController->sendMail();
+
+         })->dailyAt('00:00');
     }
 }
